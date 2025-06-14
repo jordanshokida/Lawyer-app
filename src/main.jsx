6e95163ac18ae760152491d30741ef0e9dd93e2b
javierNiz11 <nizjavier01@gmail.com>
2025-06-14 19:21:23 -0300

    Resuelvo conflicto en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-import App from "../app/App.jsx";
-=======
+
 import App from './app/App.jsx'
->>>>>>> e633b816c28414d31cf07575e351d2261c67c392
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
