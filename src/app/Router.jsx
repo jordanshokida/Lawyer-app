5dae736646e74d71ce81dfe04b80f52e810b05d2
Diego Scopetta <diegooscopetta@gmail.com>
2025-06-14 18:20:11 -0300

    Guardando cambios antes de cambiar a main

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -6,24 +6,23 @@ import SignOut from '../components/SignOut'
 import Profile from '../features/user/pages/Profile'
 import SignUp from '../components/SignUp/SignUp'
 import ProtectedRoute from '../components/ProtectedRoute'
-//import { useUserStorage } from '../stores/useUserStorage'
 import NoMatch from '../components/NoMatch'
+import TurnoForm from "../features/turnos/pages/TurnoForm"
+import FooterInfo from "../components/LandingPage/FooterInfo" 
 
 const Router = () => {
-
-  //const { user } = useUserStorage()
-
   return (
-    <Routes>
-      <Route index element={<LandingPage />} />
-      <Route path='/signin' element={<SignIn />} />
-      <Route path='/signup' element={<SignUp />} />
-      <Route path='/signout' element={<SignOut />} />
-     
-
-      
-      <Route path='*' element={<NoMatch />} />
-    </Routes >
+    <>
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signout' element={<SignOut />} />
+        <Route path="/turnos" element={<TurnoForm />} />
+        <Route path='*' element={<NoMatch />} />
+      </Routes>
+      <FooterInfo /> 
+    </>
   )
 }
 
