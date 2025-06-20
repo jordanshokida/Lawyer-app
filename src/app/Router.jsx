363fb40c3d71e1ff9b619a9fe8381f3123170dd0
jordanshokida <jordii.s@hotmail.com>
2025-06-20 02:28:13 -0300

    LawyerApp Funcional

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -1,34 +1,4 @@
-/*import React from 'react'
-import { Route, Routes } from 'react-router'
-import LandingPage from '../components/LandingPage/LandingPage'
-import SignIn from '../components/SignIn/SignIn'
-import SignOut from '../components/SignOut'
-import Profile from '../features/user/pages/Profile'
-import SignUp from '../components/SignUp/SignUp'
-import ProtectedRoute from '../components/ProtectedRoute'
-import NoMatch from '../components/NoMatch'
-import TurnoForm from "../features/turnos/pages/TurnoForm"
-import FooterInfo from "../components/LandingPage/FooterInfo" 
 
-const Router = () => {
-  return (
-    <>
-      <Routes>
-        <Route index element={<LandingPage />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/signout' element={<SignOut />} />
-        <Route path="/turnos" element={<TurnoForm />} />
-        <Route path='*' element={<NoMatch />} />
-      </Routes>
-      <FooterInfo /> 
-    </>
-  )
-}
-
-export default Router*/
-
-// src/router/Router.jsx
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
--- src/components/LandingPage/FooterInfo.jsx
+++ src/components/LandingPage/FooterInfo.jsx
@@ -1,4 +1,4 @@
-// src/components/LandingPage/FooterInfo.jsx
+
 import React from 'react';
 
 const FooterInfo = () => {
