f2b583cd2b2829c23434b1b932139e1f08e3b0f0
javierNiz11 <nizjavier01@gmail.com>
2025-06-20 17:13:27 -0300

    Cambios locales antes de mergear main

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.63gu5lcu4lg"
+    "revision": "0.fpr81f1rlj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
